9ae535555fd60ecfa1ba557eaa76642e351f57a0
Ivan de Jesus Matrille <idjmatrille@gmail.com>
2022-12-29 20:58:04 -0400

    Conexion a la BD

---
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -0,0 +1,19 @@
+﻿using ManejoPresupuesto.Models.Validaciones;
+using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models
+{
+    public class TipoCuenta
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo nombre es requerido")]
+        [PrimeraLetraMayuscula]
+        public string Nombre { get; set; }
+        public int UsuarioId { get; set; }
+         public string Orden { get; set; }
+
+        /*Prueba de otras validaciones por defecto*/
+        
+
+    }
+}
